d271153a135b8e9f42caab64ed5e3abbd55549e3
Alona Kuznietsova <116402791+acvetochka@users.noreply.github.com>
2026-08-19 13:21:51 +0200

    Create data/provider.js

---
+++ data/provider.js
@@ -0,0 +1,9 @@
+const iconProviders = {
+  skillicons: {
+    getUrl: icon => `https://skillicons.dev/icons?i=${icon}`,
+  },
+
+  syvixor: {
+    getUrl: icon => `https://skills.syvixor.com/api/icons?i=${icon}`,
+  },
+};
