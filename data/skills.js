986f1f5fb0479b773a62085f146f6c5d9cd65850
Alona Kuznietsova <116402791+acvetochka@users.noreply.github.com>
2026-08-19 13:20:12 +0200

    Create skills.js

---
+++ data/skills.js
@@ -0,0 +1,138 @@
+export const skills = {
+  frontend: {
+    title: 'Frontend',
+    provider: 'skillicons',
+
+    items: [
+      {
+        icon: 'html',
+        title: 'HTML',
+      },
+      {
+        icon: 'react',
+        title: 'React',
+      },
+      {
+        icon: 'nextjs',
+        title: 'Next.js',
+      },
+      {
+        icon: 'redux',
+        title: 'Redux',
+      },
+      {
+        icon: 'vue',
+        title: 'Vue',
+      },
+      {
+        icon: 'css',
+        title: 'CSS',
+      },
+      {
+        icon: 'sass',
+        title: 'SASS',
+      },
+      {
+        icon: 'tailwindcss',
+        title: 'TailwindCSS',
+      },
+      {
+        icon: 'emotion',
+        title: 'Emotion Styled',
+      },
+      {
+        icon: 'shadcnui',
+        title: 'ShadCN UI',
+        provider: 'syvixor',
+      },
+    ],
+  },
+
+  backend: {
+    title: 'Backend',
+    provider: 'syvixor',
+
+    items: [
+      {
+        icon: 'node',
+        title: 'Node.js',
+      },
+      {
+        icon: 'expressjs',
+        title: 'Express.js',
+      },
+      {
+        icon: 'rest',
+        title: 'REST API',
+      },
+      {
+        icon: 'mongodb',
+        title: 'MongoDB',
+      },
+      {
+        icon: 'postgresql',
+        title: 'PostgreSQL',
+      },
+      {
+        icon: 'mysql',
+        title: 'MySQL',
+      },
+      {
+        icon: 'prisma',
+        title: 'Prisma',
+      },
+    ],
+  },
+
+  utilities: {
+    title: 'Utilities',
+    provider: 'syvixor',
+
+    items: [
+      {
+        icon: 'visualstudiocode',
+        title: 'Visual Studio Code',
+      },
+      {
+        icon: 'figma',
+        title: 'Figma',
+      },
+      {
+        icon: 'github',
+        title: 'GitHub',
+      },
+      {
+        icon: 'git',
+        title: 'Git',
+      },
+      {
+        icon: 'vercel',
+        title: 'Vercel',
+      },
+      {
+        icon: 'postman',
+        title: 'Postman',
+      },
+      {
+        icon: 'trello',
+        title: 'Trello',
+      },
+      {
+        icon: 'jira',
+        title: 'Jira',
+      },
+      {
+        icon: 'visualstudio',
+        title: 'Visual Studio',
+      },
+      {
+        icon: 'jupyter',
+        title: 'Jupyter',
+      },
+      {
+        icon: 'virtualbox',
+        title: 'VirtualBox',
+      },
+    ],
+  },
+};
